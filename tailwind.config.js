108d9f63afa7894805635cbe638c311bb0c90b1b
akash-panthri <akashpanthri2000@gmail.com>
2024-01-16 10:33:37 +0530

    Initial commit (completed section 1)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -13,8 +12,20 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontSize:{
+        '2xsm':"10px",
+        xsm:"12px",
+        sm:"13px",
+        reg:"15px",
+        lg:"18px",
+        "2xl":"22px",
+        "3xl":"25px",
+        "4xl":"32px",
+        "5xl":"40px",
+        "6xl":"50px",
+        "7xl":"70px",
+      },
     },
   },
   plugins: [],
 }
-export default config
