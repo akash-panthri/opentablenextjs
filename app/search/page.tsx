f78887d2ba02dc088edd7bb9a310cbb01b602b3d
akash-panthri <akashpanthri2000@gmail.com>
2024-01-23 04:52:58 +0530

    Fetched Restaurants by Location

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,8 +3,35 @@ import React from "react";
 import Header from "./components/Header";
 import SearchSideBar from "./components/SearchSideBar";
 import SearchRestaurantCard from "./components/SearchRestaurantCard";
+import { PrismaClient } from "@prisma/client";
 
- export default function Search() {
+const prisma = new PrismaClient()
+const select= {
+   id: true,
+   name: true,
+   main_image: true,
+   price:true,
+   cuisine:true,
+   location:true,
+   slug: true,
+ }
+const fetchRestaurantByCity =async (city:string)=>{
+   if(!city) return await prisma.restaurant.findMany({select});
+   return await prisma.restaurant.findMany({
+      where: {
+        location: {
+         name: {
+            equals: city.toLowerCase()
+         },
+        }
+      },
+      select,
+  
+    });
+    
+}
+ export default async function Search({searchParams}:{searchParams:{city:string}}) {
+   const restaurant = await fetchRestaurantByCity(searchParams.city)
     return (
        <>
   <Header />
