8ecf74565f67a883126243fae07477138b4d6061
akash-panthri <akashpanthri2000@gmail.com>
2024-02-01 12:24:45 +0530

    Previously I accidentally deleted the 'pages/api' folder instead of' api' from 'app' directory ( although api folder can be kept in app directoy and it would still work with a few changes but I prefer api folder in pages directory)

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -4,8 +4,11 @@ import {NextApiRequest,NextApiResponse} from 'next';
 
 
 export default async function handler(req:NextApiRequest, res:NextApiResponse) {
-   if(req.method === "POST"){
-
+  console.log("Reaached")
+  if(req.method === "POST"){
+    
+    
+   
       res.status(200).json({
          hello:"message",
       });
