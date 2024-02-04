6afa8c6d776a72b3e0ab85b7f536ad855e9dd053
akash-panthri <akashpanthri2000@gmail.com>
2024-02-05 05:10:50 +0530

    added AuthContext

---
+++ app/context/AuthContext.tsx
@@ -0,0 +1,80 @@
+"use client";
+import React, { useState, createContext, useEffect } from "react";
+
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  city: string;
+  phone: string;
+}
+
+interface State {
+  loading: boolean;
+  error: string | null;
+  data: User | null;
+}
+
+interface AuthState extends State {
+  setAuthState: React.Dispatch<React.SetStateAction<State>>;
+}
+
+export const AuthenticationContext = createContext<AuthState>({
+  loading: false,
+  error: null,
+  data: null,
+  setAuthState: () => {},
+});
+
+export default function AuthContext({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [authState, setAuthState] = useState<State>({
+    loading: true,
+    data: null,
+    error: null,
+  });
+
+  const fetchUser = async () => {
+    setAuthState({
+      data: null,
+      error: null,
+      loading: true,
+    });
+    try {
+     
+
+    
+
+ 
+
+   
+
+   
+    } catch (error: any) {
+      setAuthState({
+        data: null,
+        error: error.response.data.errorMessage,
+        loading: false,
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  return (
+    <AuthenticationContext.Provider
+      value={{
+        ...authState,
+        setAuthState,
+      }}
+    >
+      {children}
+    </AuthenticationContext.Provider>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import NavBar from './components/NavBar'
+import AuthContext from './context/AuthContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,12 +20,14 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
       <main className="bg-gray-100 min-h-screen w-screen">
+        <AuthContext >
   <main className="max-w-screen-2xl m-auto bg-white">
     
    <NavBar />
         {children}
 
         </main>
+        </AuthContext>
   </main>
         </body>
     </html>
