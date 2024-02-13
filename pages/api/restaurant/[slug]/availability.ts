30d58f72fdd9276bfeaf90bd5ba8500894bf61bc
akash-panthri <akashpanthri2000@gmail.com>
2024-02-14 04:22:13 +0530

    Added 'availabilities' in 'availability.ts' in backend

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -36,6 +36,9 @@ export default async function handler(
       },
       select: {
         tables: true,
+        
+        open_time: true,
+        close_time: true,
       },
     });
 
@@ -89,8 +92,30 @@ export default async function handler(
       });
     });
 
-    
-    return res.json({searchTimes, bookings});
+
+    const availabilities = searchTimesWithTables
+      .map((t) => {
+        const sumSeats = t.tables.reduce((sum, table) => {
+          return sum + table.seats;
+        }, 0);
+
+        return {
+          time: t.time,
+          available: sumSeats >= parseInt(partySize),
+        };
+      })
+      .filter((availability) => {
+        const timeIsAfterOpeningHour =
+          new Date(`${day}T${availability.time}`) >=
+          new Date(`${day}T${restaurant.open_time}`);
+        const timeIsBeforeClosingHour =
+          new Date(`${day}T${availability.time}`) <=
+          new Date(`${day}T${restaurant.close_time}`);
+
+        return timeIsAfterOpeningHour && timeIsBeforeClosingHour;
+      });
+
+    return res.json({availabilities});
   }
 }
 
