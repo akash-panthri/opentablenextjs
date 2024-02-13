0e9d83a7aed771f02598c63727e804e59436b956
akash-panthri <akashpanthri2000@gmail.com>
2024-02-13 08:46:16 +0530

    Added 'bookings' in 'availability.ts' in backend

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -31,7 +31,35 @@ export default async function handler(
       });
     }
 
-    return res.json({searchTimes});
+    const bookings = await prisma.booking.findMany({
+      where: {
+        booking_time: {
+          gte: new Date(`${day}T${searchTimes[0]}`),
+          lte: new Date(`${day}T${searchTimes[searchTimes.length - 1]}`),
+        },
+      },
+      select: {
+        number_of_people: true,
+        booking_time: true,
+        tables: true,
+      },
+    });
+  
+    const bookingTablesObj: { [key: string]: { [key: number]: true } } = {};
+  
+    bookings.forEach((booking) => {
+      bookingTablesObj[booking.booking_time.toISOString()] =
+        booking.tables.reduce((obj, table) => {
+          return {
+            ...obj,
+            [table.table_id]: true,
+          };
+        }, {});
+    });
+
+
+    
+    return res.json({searchTimes, bookings});
   }
 }
 
