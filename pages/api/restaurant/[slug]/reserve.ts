f2d9f42d4d91c41cf7da10291f5b91b0ddb0d0a1
akash-panthri <akashpanthri2000@gmail.com>
2024-02-17 04:42:24 +0530

    Added feature to detemine table/s to book

--- pages/api/restaurant/[slug]/reserve.ts
+++ pages/api/restaurant/[slug]/reserve.ts
@@ -83,7 +83,34 @@ export default async function handler(
       }
     });
 
-    return res.json(searchTimeWithTables);
+    const tablesToBooks: number[] = [];
+    let seatsRemaining = parseInt(partySize);
+
+    while (seatsRemaining > 0) {
+      if (seatsRemaining >= 3) {
+        if (tablesCount[4].length) {
+          tablesToBooks.push(tablesCount[4][0]);
+          tablesCount[4].shift();
+          seatsRemaining = seatsRemaining - 4;
+        } else {
+          tablesToBooks.push(tablesCount[2][0]);
+          tablesCount[2].shift();
+          seatsRemaining = seatsRemaining - 2;
+        }
+      } else {
+        if (tablesCount[2].length) {
+          tablesToBooks.push(tablesCount[2][0]);
+          tablesCount[2].shift();
+          seatsRemaining = seatsRemaining - 2;
+        } else {
+          tablesToBooks.push(tablesCount[4][0]);
+          tablesCount[4].shift();
+          seatsRemaining = seatsRemaining - 4;
+        }
+      }
+    }
+
+    return res.json(tablesToBooks);
   }
 }
 
