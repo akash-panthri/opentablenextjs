c5fe69e01e80f44f5786f0e529d30651f5dfbaaa
akash-panthri <akashpanthri2000@gmail.com>
2024-02-16 08:48:29 +0530

    Added 'reserve' to backend

---
+++ pages/api/restaurant/[slug]/reserve.ts
@@ -0,0 +1,51 @@
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const { slug, day, time, partySize } = req.query as {
+      slug: string;
+      day: string;
+      time: string;
+      partySize: string;
+    };
+
+
+    const restaurant = await prisma.restaurant.findUnique({
+      where: {
+        slug,
+      },
+      select: {
+        tables: true,
+        open_time: true,
+        close_time: true,
+        id: true,
+      },
+    });
+
+    if (!restaurant) {
+      return res.status(400).json({
+        errorMessage: "Restaurant not found",
+      });
+    }
+
+    if (
+      new Date(`${day}T${time}`) < new Date(`${day}T${restaurant.open_time}`) ||
+      new Date(`${day}T${time}`) > new Date(`${day}T${restaurant.close_time}`)
+    ) {
+      return res.status(400).json({
+        errorMessage: "Restaurant is not open at that time",
+      });
+    }
+
+
+  }
+}
+
+// http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/reserve?day=2023-02-03&time=15:00:00.000Z&partySize=8
