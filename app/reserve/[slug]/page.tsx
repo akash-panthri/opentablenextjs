c06e92d911ba23fd83bf3461b3a6d44bd6435d01
akash-panthri <akashpanthri2000@gmail.com>
2024-02-19 05:05:29 +0530

    Rendered dynamic data to 'reserve' in frontend

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -2,20 +2,40 @@ import React from "react";
 
 import Header from "./components/Header";
 import Form from "./components/Form";
+import { PrismaClient } from "@prisma/client";
+import { notFound } from "next/navigation";
 
-export default function Reserve() {
-    return (
+const prisma = new PrismaClient();
 
-     
+const fetchRestaurantBySlug = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+  });
+
+  if (!restaurant) {
+    notFound();
+  }
+
+  return restaurant;
+};
+
+export default async function Reserve({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
+  return (
     <div className="border-t h-screen">
       <div className="py-9 w-3/5 m-auto">
         {/* HEADER */}
-       <Header />
+        <Header image={restaurant.main_image}
+          name={restaurant.name} />
         {/* HEADER */} {/* FORM */}
-       <Form />
+        <Form />
       </div>
     </div>
-
-
-    )
-};
+  );
+}
