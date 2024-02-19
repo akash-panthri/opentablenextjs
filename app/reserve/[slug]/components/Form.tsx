63f278e73c9d929b0fa52e1ffb8998b278ed34b5
akash-panthri <akashpanthri2000@gmail.com>
2024-02-19 05:36:13 +0530

    Stored formdata in 'reserve' in state

--- app/reserve/[slug]/components/Form.tsx
+++ app/reserve/[slug]/components/Form.tsx
@@ -1,37 +1,72 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from 'react'
 
 export default function Form() {
+  const [inputs, setInputs] = useState({
+    bookerFirstName: "",
+    bookerLastName: "",
+    bookerPhone: "",
+    bookerEmail: "",
+    bookerOccasion: "",
+    bookerRequest: "",
+  });
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
     <div className="mt-10 flex flex-wrap justify-between w-[660px]">
     <input
       type="text"
       className="border rounded p-3 w-80 mb-4"
       placeholder="First name"
+      name="bookerFirstName"
+      value={inputs.bookerFirstName}
+      onChange={handleChangeInput}
     />
     <input
       type="text"
       className="border rounded p-3 w-80 mb-4"
       placeholder="Last name"
+      name="bookerLastName"
+      value={inputs.bookerLastName}
+      onChange={handleChangeInput}
     />
     <input
       type="text"
       className="border rounded p-3 w-80 mb-4"
       placeholder="Phone number"
+      name="bookerPhone"
+      value={inputs.bookerPhone}
+      onChange={handleChangeInput}
     />
     <input
       type="text"
       className="border rounded p-3 w-80 mb-4"
       placeholder="Email"
+      name="bookerEmail"
+      value={inputs.bookerEmail}
+      onChange={handleChangeInput}
     />
     <input
       type="text"
       className="border rounded p-3 w-80 mb-4"
       placeholder="Occasion (optional)"
+      name="bookerOccasion"
+      value={inputs.bookerOccasion}
+      onChange={handleChangeInput}
     />
     <input
       type="text"
       className="border rounded p-3 w-80 mb-4"
       placeholder="Requests (optional)"
+      name="bookerRequest"
+      value={inputs.bookerRequest}
+      onChange={handleChangeInput}
     />
     <button
       className="bg-red-600 w-full p-3 text-white font-bold rounded disabled:bg-gray-300"
